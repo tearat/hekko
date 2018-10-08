8609b560042d8e169af2b1ea60e8ceae95d48fea
teraaret <raiiliiat@yandex.ru>
2018-10-08 18:22:07 +0500

    realtime

--- js/index.js
+++ js/index.js
@@ -2,11 +2,13 @@ var app = new Vue({
     el: "#app",
     data: {
         debug: false,
+        busy: false,
         location: 0,
         database: [],
         thread: {},
         comments: [],
-        new_comment: "",
+        new_comment_title: "",
+        new_comment_body: "",
         new_thread_title: "",
         new_thread_body: "",
     },
@@ -19,20 +21,26 @@ var app = new Vue({
             }
         },
         load_database: function() {
-            var that = this;
+            var result = [];
             $.ajax({
                 url: "/data/database.json",
                 cache: false,
                 async: false,
             }).done(function (data) {
-                that.database = data;
+                result = data;
             });
+            
+            for(i=0; i<result.length; i++) {
+//                result[i].comments = this.count_comments(result[i].id);
+                result[i].comments = 333;
+            }
+            
+            this.database = result;
         },
         load_thread(id) {
             this.location = id;
             var target = this.find_by_id(id);
             this.thread = this.database[target];
-            // comments
             this.load_comments(id);
         },
         load_comments(id) {
@@ -55,19 +63,26 @@ var app = new Vue({
             return count;  
         },
         add_comment(parent) {
-            if(this.new_comment != "") {
+            this.busy = true;
+            if(this.new_comment_body != "") {
+                if (this.new_comment_title == "") {
+                    this.new_comment_title = "Анонимус";
+                }
                 this.database.push({
                     id: this.database[this.database.length-1].id+1,
-                    title: "n/a",
-                    body: this.new_comment,
+                    title: this.new_comment_title,
+                    body: this.new_comment_body,
                     parent: parent
                 });
-                this.new_comment = "";
+                this.new_comment_title = "";
+                this.new_comment_body = "";
                 this.save_database();
                 this.load_comments(parent);
             }
+            this.busy = false;
         },
         add_thread() {
+            this.busy = true;
             var new_id;
             if(this.database.length == 0) {
                 new_id = 1;
@@ -86,15 +101,21 @@ var app = new Vue({
                 this.save_database();
                 this.load_database();
             }
+            this.busy = false;
         },
         delete_comment(comment_id, thread_id) {
+            this.busy = true;
             var target = this.find_by_id(comment_id);
             this.database.splice(target, 1);
             this.save_database();
             this.load_comments(thread_id);
+            this.busy = false;
         },
         delete_thread(thread_id) {
-            for(i=0; i<this.database.length; i++) {
+            this.busy = true;
+            
+            
+            for(i=this.database.length-1; i>=0; i--) {
                 if(this.database[i].parent == thread_id) {
                     var target = this.find_by_id(this.database[i].id);
                     this.database.splice(target, 1);
@@ -106,8 +127,10 @@ var app = new Vue({
             
             this.save_database();
             this.load_database();
+            this.busy = false;
         },
         save_database() {
+            this.busy = true;
             var that = this;
             $.ajax({
                 type: "POST",
@@ -119,10 +142,26 @@ var app = new Vue({
                     stream: JSON.stringify(that.database)
                 }
             });
+            this.busy = false;
         }
+    },
+    watch: {
+//        database: function() {
+//            this.save_database();
+//            this.load_database();
+//        }
     },
     mounted: function() {
         this.load_database();
+        var that = this;
+        setInterval(function(){ 
+            if (!that.busy) {
+                that.load_database();
+            }
+            if (that.location > 0) {
+                that.load_comments(that.location);
+            }
+        }, 500);
     }
 });
 
